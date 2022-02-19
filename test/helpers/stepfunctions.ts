5900a83162ef3f138b96029d48e683273be14017
Ken Fukuyama <kenfdev@gmail.com>
2022-02-19 10:47:09 +0900

    update

--- test/helpers/stepfunctions.ts
+++ test/helpers/stepfunctions.ts
@@ -8,7 +8,10 @@ import {
   StartExecutionCommand,
 } from '@aws-sdk/client-sfn';
 import { GenericContainer } from 'testcontainers';
-import { StateMachineTestCase } from './mock';
+import { StateMachineTestCase, StepFunctionsMockConfig } from './mock';
+import * as tmp from 'tmp-promise';
+import { promises as fs } from 'fs';
+import { StartedTestContainer } from 'testcontainers';
 
 const AWS_PARTITION = process.env.AWS_PARTITION || 'aws';
 const AWS_URL_SUFFIX = process.env.AWS_URL_SUFFIX || 'amazonaws.com';
@@ -146,3 +149,106 @@ export class SFNClientWrapper {
     });
   }
 }
+
+export const createJestTestFromMockConfig = (
+  config: StepFunctionsMockConfig,
+  aslDefinition: string
+) => {
+  return () => {
+    for (const testDefinition of config.collectStateMachineTestDefinitions()) {
+      describe(testDefinition.stateMachineName, () => {
+        let tmpDir: tmp.DirectoryResult;
+        let container: StartedTestContainer | undefined;
+
+        let client: SFNClientWrapper;
+        let stateMachineArn = '';
+
+        beforeAll(async () => {
+          const mockConfigPath = await tmp.tmpName();
+          await fs.writeFile(mockConfigPath, JSON.stringify(config.toJson()));
+          container = await createStepFunctionsLocalContainer(mockConfigPath);
+
+          // create the state machine
+          client = new SFNClientWrapper({
+            endpoint: `http://localhost:${container?.getMappedPort(8083)}`,
+          });
+          const result = await client.createStateMachine(
+            testDefinition.stateMachineName,
+            aslDefinition
+          );
+
+          // save for later usage
+          stateMachineArn = result.stateMachineArn!;
+        });
+
+        afterAll(async () => {
+          await container?.stop();
+          await tmpDir.cleanup();
+        });
+
+        for (const testCase of testDefinition.collectTestCase()) {
+          test(
+            testCase.name,
+            async () => {
+              // Arrange
+              // Act
+              const startExecutionResponse =
+                await client.startExecutionFromTestCase(
+                  stateMachineArn,
+                  testCase
+                );
+              await client.waitExecutionStop(
+                startExecutionResponse.executionArn!
+              );
+
+              // Assert
+              const { events = [] } = await client.getExecutionHistory(
+                startExecutionResponse.executionArn!
+              );
+              // console.warn(JSON.stringify(events));
+
+              const resultsMap = new Map<string, any[]>();
+              for (const e of events) {
+                if (e.stateEnteredEventDetails?.name) {
+                  const val = resultsMap.get(e.stateEnteredEventDetails.name);
+                  if (val) {
+                    resultsMap.set(e.stateEnteredEventDetails.name, [
+                      ...val,
+                      { input: e.stateEnteredEventDetails.input },
+                    ]);
+                  } else {
+                    resultsMap.set(e.stateEnteredEventDetails.name, [
+                      { input: e.stateEnteredEventDetails.input },
+                    ]);
+                  }
+                  // console.warn(JSON.stringify(e));
+                  // expect(e).toMatchSnapshot({
+                  //   timestamp: expect.anything(),
+                  // });
+                }
+                if (e.stateExitedEventDetails?.name) {
+                  const val = resultsMap.get(e.stateExitedEventDetails.name);
+                  if (val) {
+                    resultsMap.set(e.stateExitedEventDetails.name, [
+                      ...val,
+                      { output: e.stateExitedEventDetails.output },
+                    ]);
+                  } else {
+                    resultsMap.set(e.stateExitedEventDetails.name, [
+                      { output: e.stateExitedEventDetails.output },
+                    ]);
+                  }
+                }
+                // expect(e).toMatchSnapshot({
+                //   timestamp: expect.anything(),
+                // });
+              }
+              expect(resultsMap).toMatchSnapshot();
+            },
+            30_000
+          );
+        }
+      });
+    }
+  };
+};
---
+++ test/stepfunctions/sales-lead-state-machine.test.ts
@@ -0,0 +1,259 @@
+import {
+  MockedResponse,
+  StateMachineTestCase,
+  StateMachineTestDefinition,
+  StepFunctionsMockConfig,
+} from '../helpers/mock';
+
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import {
+  createJestTestFromMockConfig,
+  extractStateMachineAsls,
+} from '../helpers/stepfunctions';
+import { Duration } from 'aws-cdk-lib';
+import { SalesLeadsStateMachineConstruct } from '../../lib/sales-leads-state-machine-construct';
+
+// TODO: dummy code
+type LambdaResponse = {
+  StatusCode: number;
+  Payload: {
+    statusCode: number;
+    body: any;
+  };
+};
+type DynamoResponse = {
+  SdkHttpMetadata: {
+    HttpStatusCode: number;
+  };
+};
+type EventBridgeResponse = {
+  Payload: {
+    Entries: { EventId: string }[];
+    FailedEntryCount: number;
+  };
+};
+type ComprehendResponse = {
+  Sentiment: 'POSITIVE' | 'NEGATIVE';
+  SentimentScore: {
+    Mixed: number;
+    Negative: number;
+    Neutral: number;
+    Positive: number;
+  };
+};
+
+export const stateNames = {
+  validationStateName: 'Validation',
+  checkIdentityStateName: 'Check Identity',
+  checkAddressStateName: 'Check Address',
+  customValidationFailedStateName: 'CustomValidationFailed',
+  validationExceptionStateName: 'ValidationException',
+  detectSentimentStateName: 'DetectSentiment',
+  isPositiveSentimentStateName: 'Is Positive Sentiment?',
+  negativeSentimentDetectedStateName: 'NegativeSentimentDetected',
+  addToFollowUpStateName: 'Add to FollowUp',
+  customerAddedToFollowupStateName: 'CustomerAddedToFollowup',
+} as const;
+
+type StateNameKeys = keyof typeof stateNames;
+type StateName = typeof stateNames[StateNameKeys];
+
+const stateMachineName = 'LocalTesting';
+
+const checkIdentityLambdaMockedSuccess = new MockedResponse(
+  'CheckIdentityLambdaMockedSuccess'
+).return<LambdaResponse>({
+  StatusCode: 200,
+  Payload: {
+    statusCode: 200,
+    body: JSON.stringify({
+      approved: true,
+      message: 'identity validation passed',
+    }),
+  },
+});
+
+const checkAddressLambdaMockedSuccess = new MockedResponse(
+  'CheckAddressLambdaMockedSuccess'
+).return<LambdaResponse>({
+  StatusCode: 200,
+  Payload: {
+    statusCode: 200,
+    body: JSON.stringify({
+      approved: true,
+      message: 'address validation passed',
+    }),
+  },
+});
+
+const addToFollowUpSuccess = new MockedResponse(
+  'AddToFollowUpSuccess'
+).return<DynamoResponse>({
+  SdkHttpMetadata: {
+    HttpStatusCode: 200,
+  },
+});
+
+const customerAddedToFollowupSuccess = new MockedResponse(
+  'CustomerAddedToFollowupSuccess'
+).return({
+  StatusCode: 200,
+  Payload: {
+    statusCode: 200,
+  },
+});
+
+const checkIdentityLambdaMockedThrowError = new MockedResponse(
+  'CheckIdentityLambdaMockedThrowError'
+).throw('CustomValidationError', 'Check Identity Validation Failed', 3);
+
+const checkAddressLambdaMockedThrowExceptionSuccess = new MockedResponse(
+  'CheckAddressLambdaMockedThrowExceptionSuccess'
+).throw('CustomAddressValidationError', 'Address Validation Exception');
+
+const customValidationFailedPutEventSuccess = new MockedResponse(
+  'CustomValidationFailedPutEventSuccess'
+).return<EventBridgeResponse>({
+  Payload: {
+    Entries: [
+      {
+        EventId: 'abc123',
+      },
+    ],
+    FailedEntryCount: 0,
+  },
+});
+
+const validationExceptionPutEventSuccess = new MockedResponse(
+  'ValidationExceptionPutEventSuccess'
+).return<EventBridgeResponse>({
+  Payload: {
+    Entries: [
+      {
+        EventId: 'abc123',
+      },
+    ],
+    FailedEntryCount: 0,
+  },
+});
+
+const detectSentimentPositive = new MockedResponse(
+  'DetectSentimentPositive'
+).return<ComprehendResponse>({
+  Sentiment: 'POSITIVE',
+  SentimentScore: {
+    Mixed: 0.00012647535,
+    Negative: 0.00008031699,
+    Neutral: 0.0051454515,
+    Positive: 0.9946478,
+  },
+});
+
+const detectSentimentNegative = new MockedResponse(
+  'DetectSentimentNegative'
+).return<ComprehendResponse>({
+  Sentiment: 'NEGATIVE',
+  SentimentScore: {
+    Mixed: 0.00012647535,
+    Positive: 0.00008031699,
+    Neutral: 0.0051454515,
+    Negative: 0.9946478,
+  },
+});
+
+const negativeSentimentDetectedSuccess = new MockedResponse(
+  'NegativeSentimentDetectedSuccess'
+).return<EventBridgeResponse>({
+  Payload: {
+    Entries: [
+      {
+        EventId: 'abc123',
+      },
+    ],
+    FailedEntryCount: 0,
+  },
+});
+
+const detectSentimentRetryOnErrorWithSuccess = new MockedResponse(
+  'DetectSentimentRetryOnErrorWithSuccess'
+)
+  .throw(
+    'InternalServerException',
+    'Server Exception while calling DetectSentiment API in Comprehend Service',
+    2
+  )
+  .return<ComprehendResponse>({
+    Sentiment: 'POSITIVE',
+    SentimentScore: {
+      Mixed: 0.00012647535,
+      Negative: 0.00008031699,
+      Neutral: 0.0051454515,
+      Positive: 0.9946478,
+    },
+  });
+
+const stateMachineTestDefinition = new StateMachineTestDefinition(
+  stateMachineName
+).addTestCase(
+  new StateMachineTestCase<StateName>('HappyPathTest')
+    .withInput({
+      data: {
+        firstname: 'Jane',
+        lastname: 'Doe',
+        identity: {
+          email: 'jdoe@example.com',
+          ssn: '123-45-6789',
+        },
+        address: {
+          street: '123 Main St',
+          city: 'Columbus',
+          state: 'OH',
+          zip: '43219',
+        },
+        comments:
+          'I am glad to sign-up for this service. Looking forward to different options.',
+      },
+    })
+    .addMockedState(
+      stateNames.checkIdentityStateName,
+      checkIdentityLambdaMockedSuccess
+    )
+    .addMockedState(
+      stateNames.checkAddressStateName,
+      checkAddressLambdaMockedSuccess
+    )
+    .addMockedState(
+      stateNames.detectSentimentStateName,
+      detectSentimentPositive
+    )
+    .addMockedState(stateNames.addToFollowUpStateName, addToFollowUpSuccess)
+    .addMockedState(
+      stateNames.customerAddedToFollowupStateName,
+      customerAddedToFollowupSuccess
+    )
+);
+
+const config = new StepFunctionsMockConfig();
+config.addTestDefinition(stateMachineTestDefinition);
+
+const stack = new cdk.Stack();
+new SalesLeadsStateMachineConstruct(stack, 'SalesLeadsStateMachine', {
+  validationStateName: 'Validation',
+  checkIdentityStateName: 'Check Identity',
+  checkAddressStateName: 'Check Address',
+  customValidationFailedStateName: 'CustomValidationFailed',
+  validationExceptionStateName: 'ValidationException',
+  detectSentimentStateName: 'DetectSentiment',
+  isPositiveSentimentStateName: 'Is Positive Sentiment?',
+  negativeSentimentDetectedStateName: 'NegativeSentimentDetected',
+  addToFollowUpStateName: 'Add to FollowUp',
+  customerAddedToFollowupStateName: 'CustomerAddedToFollowup',
+  stateMachineName: stateMachineName,
+  retryInterval: Duration.seconds(0),
+});
+const cfnStackJson = Template.fromStack(stack).toJSON();
+// extract the Asl part
+const asls = extractStateMachineAsls(cfnStackJson);
+
+describe('SalesLeasStateMachine', createJestTestFromMockConfig(config, asls[0]));
